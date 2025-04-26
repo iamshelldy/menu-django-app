120f3dc56e4ec698c84ad67a55813ec511938b34
Denis Raetskii <iamshelldy@gmail.com>
2025-04-27 01:39:30 +0500

    feat: added menu data to database; added `styles.css` to menu.

--- app/menu/urls.py
+++ app/menu/urls.py
@@ -4,6 +4,6 @@ from menu import views
 
 
 urlpatterns = [
-    path('', views.MenuView.as_view(), name='menu-item-default'),
     path('<path:item_slug>/', views.MenuView.as_view(), name='menu-item'),
+    path('', views.MenuView.as_view(), name='menu-item-default'),
 ]
